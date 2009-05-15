b93a930ebb01e42f2ab62ba17c8dd93357591d8c
George Plsek <george@gtekg.com>
2009-05-15 10:54:24 -0700

    updated header

--- header.php
+++ header.php
@@ -160,13 +160,29 @@ jQuery.noConflict();
 							}  
 						?>
                 </ul>
-	  <form method="get" id="searchform" action="<?php bloginfo('home'); ?>/"> 
-	 	                      <fieldset>
+
+
+				<form action="http://hotparks.com/search" id="cse-search-box">
+				  <fieldset>
+				    <input type="hidden" name="cx" value="015459459567427136655:6whklon_3ea" />
+				    <input type="hidden" name="cof" value="FORID:10" />
+				    <input type="hidden" name="ie" value="UTF-8" />
+				    <input type="text" name="q" size="31" value="SEARCH"/>
+				    <!-- <input type="submit" name="sa" value="Search" /> -->
+
+				 </fieldset>
+				</form>
+
+				<script type="text/javascript" src="http://www.google.com/coop/cse/brand?form=cse-search-box&lang=en"></script>
+
+
+	  <!-- <form method="get" id="searchform" action="<?php bloginfo('home'); ?>/"> 
+	 	                      
 	 	                          <!--<label>SEARCH</label>-->
-	 	                          <input type="text" value="SEARCH" onclick="this.value='';" name="s" id="s" />
-	 	  					
-	 	                      </fieldset>
-	 	                  </form>
+	 	                          <!-- <input type="text" value="SEARCH" onclick="this.value='';" name="s" id="s" />
+	 	                          	 	  					
+	 	                          	 	                      
+	 	                          	 	                  </form> -->
 	
 	   					<div class="signin">
 					                    <?php
