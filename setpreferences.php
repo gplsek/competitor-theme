10568151ef509c516b5447e0968249552247a754
George Plsek <george@gtekg.com>
2009-05-19 17:46:55 -0700

    updated the url

--- setpreferences.php
+++ setpreferences.php
@@ -4,7 +4,7 @@ foreach($_POST['sport'] as $key => $value){
 $cookie_sport .= $value . "|";}
 //trim the last | from the end
 $cookie_sport = substr($cookie_sport, 0, -1);
-setcookie("Sport", $cookie_sport,time()+(60*60*24*365),"/",".tworld.com");
+setcookie("Sport", $cookie_sport,time()+(60*60*24*365),"/");
 header("Location: http://{$_SERVER['SERVER_NAME']}/");
 
 
