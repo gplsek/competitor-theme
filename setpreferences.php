0b1b68c0f15fd05e6a0d0245e6f0a880551ccf19
George Plsek <george@gtekg.com>
2009-05-27 11:17:55 -0700

    update custom

--- setpreferences.php
+++ setpreferences.php
@@ -4,7 +4,7 @@ foreach($_POST['sport'] as $key => $value){
 $cookie_sport .= $value . "|";}
 //trim the last | from the end
 $cookie_sport = substr($cookie_sport, 0, -1);
-setcookie("Sport", $cookie_sport,time()+(60*60*24*365),"http://tworld.com");
+setcookie("Sport", $cookie_sport,time()+(60*60*24*365),"/");
 
 if (isset($_COOKIE['Sport']))
 {
