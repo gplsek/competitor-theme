b6bd27ef10275ace9e9d9a516414e0e6a8a56f8c
George Plsek <george@gtekg.com>
2009-07-08 15:44:18 -0700

    update layout

--- layouts/widget_layout.php
+++ layouts/widget_layout.php
@@ -10,6 +10,18 @@ global $user_ID, $wpdb, $post, $current_category;
 	<div id="left-velonews">
 		<div class="latest-tech">
 			<h3>Latest Tech</h3>
+			
+			<ul class="list1">
+				<?php 
+				    $featcat = 'tech';
+					$the_query = new WP_Query('category_name='.$featcat.'&showposts=5&orderby=post_date&order=desc');		
+					while ($the_query->have_posts()) : $the_query->the_post(); $do_not_duplicate = $post->ID;
+				?>
+
+					<li><a title="Permanent Link to <?php the_title(); ?>" href="<?php the_permalink() ?>" rel="bookmark"><?php the_title(); ?></a>
+					<label class="date"><?php the_date('M j'); ?></label><?php //the_advanced_excerpt(); ?></li>
+					
+					
 		</div>
 		<div class="ad-velonews">
 			<h3>Ad</h3>
@@ -18,6 +30,20 @@ global $user_ID, $wpdb, $post, $current_category;
 	<div id="right-velonews">
 		<div class="latest-news">
 			<h3>Latest News</h3>
+			
+			<ul class="list1">
+				<?php 
+				    $featcat = 'news';
+					$the_query = new WP_Query('category_name='.$featcat.'&showposts=5&orderby=post_date&order=desc');		
+					while ($the_query->have_posts()) : $the_query->the_post(); $do_not_duplicate = $post->ID;
+				?>
+
+					<li><a title="Permanent Link to <?php the_title(); ?>" href="<?php the_permalink() ?>" rel="bookmark"><?php the_title(); ?></a>
+					<label class="date"><?php the_date('M j'); ?></label><?php //the_advanced_excerpt(); ?></li>
+
+				<?php endwhile; ?>	
+				<li><a href="/category/<?= $featcat;?>/">+ More</a>	</li>
+			</ul>
 		</div>
 	</div>
 </div>
