61b0b944d05f982041a55d2e56ed08cdcd093a4c
George Plsek <george@gtekg.com>
2009-07-14 12:32:06 -0700

    add side bar

--- layouts/widget_layout.php
+++ layouts/widget_layout.php
@@ -1,9 +1,19 @@
 <div id="content_left">
 <div class="widget">
+	<?php if (function_exists('dynamic_sidebar') && dynamic_sidebar('content_left') ) : else : ?>		
+		
+	
+		
+		<?php endif; ?>
 </div>
 </div>
 
 <div id="content_right">
 <div class="widget">
+	<?php if (function_exists('dynamic_sidebar') && dynamic_sidebar('content_right') ) : else : ?>		
+		
+	
+		
+		<?php endif; ?>
 </div>
 </div>
