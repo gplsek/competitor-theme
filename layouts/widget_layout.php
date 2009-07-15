86f35b67ef84f686aa4b6b8d06d1d8fbec44e967
George Plsek <george@gtekg.com>
2009-07-15 17:21:44 +0000

    update velo

--- layouts/widget_layout.php
+++ layouts/widget_layout.php
@@ -1,19 +1,19 @@
-<div id="content_left">
-<div class="widget">
-	<?php if (function_exists('dynamic_sidebar') && dynamic_sidebar('content_left') ) : else : ?>		
-		
-	
-		
-		<?php endif; ?>
-</div>
-</div>
+<div id="velonews-content">
 
-<div id="content_right">
-<div class="widget">
-	<?php if (function_exists('dynamic_sidebar') && dynamic_sidebar('content_right') ) : else : ?>		
-		
-	
-		
-		<?php endif; ?>
-</div>
-</div>
+    <div id="content_left">
+        <?php if (function_exists('dynamic_sidebar') && dynamic_sidebar('content_left') ) : else : ?>		
+            
+        
+            
+            <?php endif; ?>
+    </div>
+    
+    <div id="content_right">
+        <?php if (function_exists('dynamic_sidebar') && dynamic_sidebar('content_right') ) : else : ?>		
+            
+        
+            
+            <?php endif; ?>
+    </div>
+    
+</div>    
